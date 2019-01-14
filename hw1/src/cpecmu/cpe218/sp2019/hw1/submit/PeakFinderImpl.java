0b8425ba9f3db7a43e693810734e0a2a86fc24e2
mrkwskiti <markdog_@hotmail.com>
2019-01-14 23:33:16 +0700

    Optimizing peak finder

--- hw1/src/cpecmu/cpe218/sp2019/hw1/submit/PeakFinderImpl.java
+++ hw1/src/cpecmu/cpe218/sp2019/hw1/submit/PeakFinderImpl.java
@@ -14,19 +14,18 @@ public class PeakFinderImpl implements PeakFinder {
 
     @Override
     public int peakPosition(int[] a) {
-        int pos = a.length/2;
-        int head = 0;
-        int tail = a.length-1;
-        return peakPosition(a, pos, head, tail);
+        return peakPosition(a, 0, a.length - 1);
     }
 
-    public int peakPosition(int[] a, int pos, int head, int tail)
+    public int peakPosition(int[] a, int head, int tail)
     {
+        // find middle position form head and tail
+        int pos = head + (tail - head)/2;
+        // peak finding algorithm
         if(tail == head) return pos;
         else if(a[pos] >= a[pos-1] && a[pos] >= a[pos+1]) return pos;
-        else if(a[pos-1] >= a[pos+1]) return peakPosition(a, ((pos-1-head)/2)+head, head, pos-1);
-        else if(a[pos-1] <= a[pos+1]) return peakPosition(a, ((tail-pos+1)/2)+pos, pos+1, tail);
-        return -1;
+        else if(a[pos-1] >= a[pos+1]) return peakPosition(a, head, pos-1);
+        else /* a[pos-1] < a[pos+1] */ return peakPosition(a, pos+1, tail);
     }
 
     public static void main(String[] args) {
